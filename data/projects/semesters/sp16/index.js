2a69bb0f32b6bd5817eb9cec91c0c8dc7985ed71
Daniel Tian <dtian.95@gmail.com>
2020-02-08 20:43:53 -0500

    added 2016s

--- data/projects/semesters/sp16/index.js
+++ data/projects/semesters/sp16/index.js
@@ -1,15 +1,23 @@
-import JuvenileSentence from "./jscc";
-import Ican from "./ican";
+import Maps from "./maps4all";
+import Busy from "./busybooth";
+import Reading from "./reading";
+import Transcribe from "./transcribe4all";
 
 // Fa14 Project Data
 export default {
   semester: "Spring 2016",
   projects: [
     {
-      ...JuvenileSentence
+      ...Maps
     },
     {
-      ...Ican
+      ...Busy
+    },
+    {
+      ...Reading
+    },
+    {
+      ...Transcribe
     }
   ]
 };
